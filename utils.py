8ab31afd1bf2346e1039352e39e58c9f2a11598e
johnvelgakis <johnvelgakis@gmail.com>
2024-04-26 08:04:41 +0300

    update

--- utils.py
+++ utils.py
@@ -1,17 +1,8 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import f_classif
-from sklearn.metrics import mean_squared_error
-import tensorflow as tf
-from tensorflow.python.keras import models
-from tensorflow.python.keras.layers import Dense
-from tensorflow.python.keras.layers import Dropout
-from tensorflow.keras import models, layers
-import matplotlib.pyplot as plt
+import numpy as np
 
 
 # Vectorization parameters
-# Range (inclusive) of n-gram sizes for tokenizing text.
 NGRAM_RANGE = (1, 2)
 
 # Limit on the number of features. We use the top 2K features.
@@ -65,63 +56,5 @@ def ngram_vectorize(train_texts, test_texts):
 
 
 
-def plot_metrics(history):
-    # Extract the training and validation metrics
-    train_loss = history.history['loss']
-    val_loss = history.history['val_loss']
-    train_mae = history.history['mae']
-    val_mae = history.history['val_mae']
-
-    # Create subplots
-    fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(8, 12))
-
-    # Plot the loss curves
-    axes[0].plot(train_loss, label='Training Loss')
-    axes[0].plot(val_loss, label='Validation Loss')
-    axes[0].set_xlabel('Epochs')
-    axes[0].set_ylabel('Loss')
-    axes[0].set_title('Training and Validation Loss')
-    axes[0].legend()
-
-    # Plot the accuracy curves
-    axes[1].plot(train_mae, label='Training Mean Absolute Error')
-    axes[1].plot(val_mae, label='Validation Mean Absolute Error')
-    axes[1].set_xlabel('Epochs')
-    axes[1].set_ylabel('Accuracy')
-    axes[1].set_title('Training and Validation Accuracy')
-    axes[1].legend()
-
-    # Adjust spacing between subplots
-    plt.subplots_adjust(hspace=0.4)
-
-    # Display the plot
-    plt.show()
 
 
-
-def create_regression_mlp(input_shape, x_train, train_labels, epochs, validation_data, batch_size):
-    # Define the model architecture
-    model = models.Sequential([
-        layers.Dense(16, activation='relu', input_shape=input_shape),
-        layers.Dense(1)  # Output layer with single unit for regression
-    ])
-    
-    # Compile the model
-    #opt = tf.keras.optimizers.Nadam(learning_rate=0.001, beta_1=0.9, beta_2=0.999)
-    opt='adam'
-    model.compile(optimizer=opt, loss='mean_squared_error')
-    
-    # Train the model
-    history = model.fit(
-        x_train,
-        train_labels,
-        epochs=epochs,
-        validation_data=validation_data,
-        verbose=2,
-        batch_size=batch_size
-    )
-    
-    # Print results
-    print('Validation loss:', history.history['val_loss'][-1])
-    
-    return model
